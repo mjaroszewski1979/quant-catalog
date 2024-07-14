ae56ffd649c6743dea4fcc5b4f3e1d05e8bdf99d
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-14 19:45:16 +0200

    Update apps.py

--- catalog/apps.py
+++ catalog/apps.py
@@ -1,6 +1,14 @@
+# Import AppConfig class from Django for configuring application settings
 from django.apps import AppConfig
 
 
 class CatalogConfig(AppConfig):
+    """
+    Configuration class for the Catalog application.
+    Inherits from Django's AppConfig to set application-specific configurations.
+    """
+
+    # Specifies the default field type for auto-incrementing primary keys
     default_auto_field = 'django.db.models.BigAutoField'
+    # Defines the name of the application, which is used by Django to identify the app
     name = 'catalog'
